091a3c365460bbcdcb319ba179ee8a1de7ce7d56
Brandon McCaig <bamccaig@gmail.com>
2009-11-22 15:33:05 -0500

    Fixing copyright notice and scripts.
    The copyright notice still had '#' comment markers from a Python project
    they were copied from. I had fixed source files but forgotten to change
    the script, and when I added the copyright to Range.*pp the incorrect
    comment markers were again added. Fixed in both the script and source
    files.
    
    Also added the execute bits to the two scripts, though I'm unsure
    exactly how Cygwin (where this project originated) will affect that.
    They were executable before I created the git repository so I can only
    assume Cygwin's git somehow removed the execute bits. Or maybe it was
    Cygwin... Who knows... I recall a Git config variable to ignore that
    entirely. I might need to set that, but for now it appears fixed.

--- Range.hpp
+++ Range.hpp
@@ -1,22 +1,22 @@
-#
-# rot is a cypher utility implementing the rudimentary alphabetic rotation cypher.
-# Copyright (C) 2009 Brandon McCaig
-#
-# This file is part of rot.
-#
-# rot is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
-#
-# rot is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with rot.  If not, see <http://www.gnu.org/licenses/>.
-#
+//
+// rot is a cypher utility implementing the rudimentary alphabetic rotation cypher.
+// Copyright (C) 2009 Brandon McCaig
+//
+// This file is part of rot.
+//
+// rot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// rot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rot.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef RANGE_HPP
     #define RANGE_HPP
